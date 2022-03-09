3d7946ce82143959abd5fd061ce4ce54226f4a2c
Temitayo Osunkiyesi <osunkiyesitayo@gmail.com>
2022-03-09 01:50:30 +0100

    Update jwt.js

--- utils/jwt.js
+++ utils/jwt.js
@@ -1,11 +1,19 @@
-const sendToken = (user, statusCode, res) => {
+const sendToken = (user, statusCode, res, login) => {
     const token = user.getJwtToken()
 
     const options = {
         expires: new Date(Date.now() + process.env.COOKIE_EXPIRES_TIME * 24 * 60 * 60 * 1000),
         httpOnly: true
     }
-
+    let details
+    if (login) {
+        details = {
+            name: user.name,
+            phone: user.phone,
+            email: user.email,
+            address: user.address     
+        }
+    }
     res.status(statusCode).cookie('token', token, options).json({
         success: true,
         token,
