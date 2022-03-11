b2ec9b86d7206df04aca28b692b38665e1676e79
dami-laare <osunkiyesitayo@gmail.com>
2022-03-11 08:14:43 +0100

    jhnt

--- server.js
+++ server.js
@@ -26,11 +26,4 @@ app.get('/', async (req, res) => {
 // Handling unhandled promise rejections
 process.on('unhandledRejection', (err) => {
     console.log(`ERROR: ${err.message}`);
-    console.log(`Shutting down server due to unhandled rejection`)
-    server.close(() => {
-        console.info("Server closed. Restarting.");
-
-        app.listen(process.env.PORT || 4000, () => {
-            console.log(`Server started on PORT: ${process.env.PORT || 4000}`)
-        });
-})})
+})
